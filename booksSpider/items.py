300759c0356ced94f57fc4d6ad0f8c60eeb71e0c
thgabell <thgabell@student.42.fr>
2024-01-24 23:34:41 +0100

    final

---
+++ booksSpider/items.py
@@ -0,0 +1,17 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class BookItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    title = scrapy.Field()
+    price = scrapy.Field()
+    rank = scrapy.Field()
+    availability = scrapy.Field()
+    category = scrapy.Field()
+    url = scrapy.Field()
---
+++ booksSpider/spiders/spider.py
@@ -0,0 +1,37 @@
+import scrapy
+from urllib.parse import urljoin
+from booksSpider.items import BookItem
+
+class BooksSpider(scrapy.Spider):
+    """
+    Simple spider to scrape books's data from https://books.toscrape.com/
+    """
+    name = "bookss"
+    start_urls = ["https://books.toscrape.com/index.html"]
+
+    def parse(self, response):
+        """
+        Parse index.html to get all category's urls and follow them to scrape each one.
+        """
+        category_links = response.xpath("//ul[@class='nav nav-list']/li/ul/li[*]/a/@href").getall()
+
+        for category_link in category_links:
+            yield response.follow(category_link, callback=self.parse_category)
+
+    def parse_category(self, response):
+        """
+        Parse category page and extract books's datas. Datas are stored in scrapy items.
+        """
+        for book in response.xpath("//article[@class='product_pod']"):
+            item = BookItem()
+            item["title"] = book.xpath("h3/a/@title").get()
+            item["price"] = book.xpath("div[@class='product_price']/p[@class='price_color']/text()").get()
+            item["rank"] = book.xpath("p/@class").get().split(" ")[1]
+            item["availability"] = bool(book.xpath("div[@class='product_price']/p[@class='instock availability']/i[@class='icon-ok']"))
+            item["category"] = response.xpath("//div[@class='page-header action']/h1/text()").get()
+            item["url"] = urljoin(response.url, book.xpath("h3/a/@href").get())
+            yield item
+
+        next_page = response.xpath("//li[@class='next']/a/@href").get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse_category)
